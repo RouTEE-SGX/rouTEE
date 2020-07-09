933eaec00f399597e3a6d508b93adc5084ff7b4d
ElectricPanda <jmlee@altair.snu.ac.kr>
2020-07-09 09:11:25 +0000

    add OP_PRINT_CHANNELS

--- tee/App/network.h
+++ tee/App/network.h
@@ -2,8 +2,9 @@
 #define _NETWORK_H_
 
 // opcodes for the command
-#define OP_HELLO_WORLD  97
+#define OP_HELLO_WORLD  97 // = a
 #define OP_PUSH_A   98
 #define OP_ADD_CHANNEL   99
+#define OP_PRINT_CHANNELS   100
 
 #endif  // _NETWORK_H_
