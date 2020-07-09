3cf9762e64747a57fc778f1acf1c63957f7a28d1
ElectricPanda <jmlee@altair.snu.ac.kr>
2020-07-09 09:13:13 +0000

    add ecall_print_channels() function

--- tee/Enclave/Enclave.cpp
+++ tee/Enclave/Enclave.cpp
@@ -7,6 +7,10 @@
 #include "channel.h"
 #include "errors.h"
 
+// user address to the user's channels
+map< string, vector<Channel *> > addresses_to_channels;
+vector<Channel *> channels;
+
 // invoke OCall to display the enclave buffer to the terminal screen
 void printf(const char *fmt, ...) {
 
@@ -24,5 +28,31 @@ void printf_helloworld() {
 }
 
 int ecall_add_channel() {
+    // temp code
+
+    Channel *ch = new Channel;
+    ch->addresses[0] = "0x1234";
+    ch->addresses[1] = "0xabcd";
+    ch->balances[0] = 10;
+    ch->balances[1] = 20;
+
+    printf("new channel added: %s:%llu / %s:%llu\n", ch->addresses[0], ch->balances[0], ch->addresses[1], ch->balances[1]);
+
+    channels.push_back(ch);
     return NO_ERROR;
+}
+
+void ecall_print_channels() {
+    for (vector<Channel *>::iterator iter = channels.begin(); iter != channels.end(); iter++){
+        printf("print channel info: %s\n", (*iter)->to_string().c_str());
+    }
+    return;
+}
+
+void ecall_seal_channels() {
+    
+}
+
+void ecall_unseal_channels() {
+    
 }
--- tee/Enclave/Enclave.h
+++ tee/Enclave/Enclave.h
@@ -13,6 +13,9 @@ void printf(const char *fmt, ...);
 // Ecalls
 void printf_helloworld();
 int ecall_add_channel();            // add his channel to rouTEE (= deposit)
+void ecall_print_channels();        // print all channels
+void ecall_seal_channels();         // seal all channels
+void ecall_unseal_channels();       // unseal all channels
 int ecall_remove_channel();         // remove his channel from rouTEE (= withdraw)
 int ecall_do_payment();             // request for his channel payment
 int ecall_do_multihop_payment();    // request for multi hop payment (need routing fee)
