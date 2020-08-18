be379eeb489adecd731aff29035a37847ad93dfb
ElectricPanda <jmlee@altair.snu.ac.kr>
2020-08-18 06:50:14 +0000

    refactoring & add command execution time logging

--- tee/client/client.py
+++ tee/client/client.py
@@ -3,6 +3,7 @@
 
 import socket
 from datetime import datetime
+import csv
 
 HOST = "127.0.0.1"
 PORT = 7223
@@ -12,22 +13,113 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # connect to the server
 client_socket.connect((HOST, PORT))
 
-# send cmd to server
-command = "k"
-startTime = datetime.now()
-client_socket.sendall(command.encode())
+def runScript(fileName):
+    print("run script\n")
 
-# get response from server
-data = client_socket.recv(1024)
-input()
-elapsed = datetime.now() - startTime
-print elapsed
-print 'Received:', data
+    f = open("scripts/"+fileName, 'r')
+    rdr = csv.reader(f)
 
-elapsedMicrosec = elapsed.seconds*1000000 + elapsed.microseconds
-elapsedMillisec = elapsedMicrosec/1000.0
-elapsedSec = elapsedMillisec/1000.0
-print "elapsed:", elapsedMicrosec, "microsec /", elapsedMillisec, "millisec /", elapsedSec, "sec"
+    # command count
+    paymentCount = 0
+    settleCount = 0
+    createChannelCount = 0
 
-# close socket
-client_socket.close()
+    # command execution time sum (microsec)
+    paymentTimeSum = 0
+    settleTimeSum = 0
+    createChannelTimeSum = 0
+
+    totalStartTime = datetime.now()
+    elapsedTimeSum = 0
+    for command in rdr:
+        print(command[0])
+        if len(command) == 0:
+            continue
+
+        # send command to server
+        startTime = datetime.now()
+        client_socket.sendall(command[0].encode())
+
+        # get response from server
+        data = client_socket.recv(1024)
+        elapsed = datetime.now() - startTime
+        print(elapsed)
+        print('Received:', data.decode())
+
+        # check the result
+        if data.decode() != "SUCCESS":
+            print("ERROR: command failed\n")
+            return
+
+        # print elapsed time
+        elapsedMicrosec = elapsed.seconds * 1000000 + elapsed.microseconds
+        elapsedMillisec = elapsedMicrosec / 1000.0
+        elapsedSec = elapsedMillisec / 1000.0
+        elapsedTimeSum = elapsedTimeSum + elapsedMicrosec
+        print("elapsed:", elapsedMicrosec, "microsec /", elapsedMillisec, "millisec /", elapsedSec, "sec\n")
+
+        # logging execution time info
+        if command[0][0] == 'j':
+            paymentCount = paymentCount + 1
+            paymentTimeSum = paymentTimeSum + elapsedMicrosec
+        elif command[0][0] == 'l':
+            settleCount = settleCount + 1
+            settleTimeSum = settleTimeSum + elapsedMicrosec
+        elif command[0][0] == 'm,':
+            createChannelCount = createChannelCount + 1
+            createChannelTimeSum = createChannelTimeSum + elapsedMicrosec
+
+    totalElapsed = datetime.now() - totalStartTime
+    print("run script elapsed time:", totalElapsed, "\n")
+    print("elapsed time sum:", elapsedTimeSum, "ms")
+
+    try:
+        print("payment count:", paymentCount, "/ payment execution time:", paymentTimeSum, "( avg time:", paymentTimeSum/paymentCount, "ms )")
+    except:
+        print("payment count:", 0, "/ payment execution time:", 0, "( avg time:", 0, "ms )")
+    try:
+        print("settle count:", settleCount, "/ settle execution time:", settleTimeSum, "( avg time:", settleCount/settleTimeSum, "ms )")
+    except:
+        print("settle count:", 0, "/ settle execution time:", 0, "( avg time:", 0, "ms )")
+    try:
+        print("create channel count:", createChannelCount, "/ create channel execution time:", createChannelTimeSum, "( avg time:", createChannelTimeSum/createChannelCount, "ms )")
+    except:
+        print("create channel count:", 0, "/ create channel execution time:", 0, "( avg time:", 0, "ms )")
+
+    print("")
+    return
+
+
+
+if __name__ == "__main__":
+
+    while (True):
+        command = input("input command: ")
+        if len(command) == 0:
+            # ignore '\n'
+            print("")
+            continue
+        
+        if command[0] == 's':
+            # execute script
+            runScript(command)
+            continue
+        
+        # send command to server
+        startTime = datetime.now()
+        client_socket.sendall(command.encode())
+
+        # get response from server
+        data = client_socket.recv(1024)
+        elapsed = datetime.now() - startTime
+        print(elapsed)
+        print('Received:', data.decode())
+
+        # print elapsed time
+        elapsedMicrosec = elapsed.seconds * 1000000 + elapsed.microseconds
+        elapsedMillisec = elapsedMicrosec / 1000.0
+        elapsedSec = elapsedMillisec / 1000.0
+        print("elapsed:", elapsedMicrosec, "microsec /", elapsedMillisec, "millisec /", elapsedSec, "sec\n")
+    
+    # close socket
+    client_socket.close()
