f86188844322a70e6dc05316f19f12130730fd68
ElectricPanda <jmlee@altair.snu.ac.kr>
2020-10-22 06:42:39 +0000

    add sleep() & split()

--- tee/Enclave/utils.cpp
+++ tee/Enclave/utils.cpp
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "utils.h"
 
 string long_long_to_string(unsigned long long num) {
@@ -9,4 +8,28 @@ string long_long_to_string(unsigned long long num) {
 
 unsigned long long string_to_long_long(string str_num) {
     return strtoull(str_num.c_str(), NULL, 10);
+}
+
+void pseudo_sleep(long long int sec) {
+    for (long long int i = 0; i < sec * 1000000000; i++) {
+        // just time consuming work
+        // almost same as --> sleep(sec);
+    }
+}
+
+void split(const string &cmd, vector<string> &params, char delimiter) {
+    size_t pos = cmd.find(delimiter);
+    size_t initialPos = 0;
+    params.clear();
+
+    // Decompose statement
+    while(pos != string::npos) {
+        params.push_back(cmd.substr(initialPos, pos - initialPos));
+        initialPos = pos + 1;
+
+        pos = cmd.find(delimiter, initialPos);
+    }
+
+    // Add the last one
+    params.push_back(cmd.substr(initialPos, std::min(pos, cmd.size()) - initialPos + 1));
 }
--- tee/Enclave/utils.h
+++ tee/Enclave/utils.h
@@ -1,12 +1,18 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
+#include <stdio.h>
+
 #include <string>
+#include <vector>
+using std::vector;
 using std::string;
 
 #define MAX_NUM_LENGTH 30
 
 string long_long_to_string(unsigned long long num);
 unsigned long long string_to_long_long(string str);
+void pseudo_sleep(long long int sec);
+void split(const string &cmd, vector<string> &params, char delimiter);
 
 #endif  // _UTILS_H_
