31c00d6f1c63bdf83c391b6d00739658b5d7704f
ElectricPanda <jmlee@altair.snu.ac.kr>
2020-10-22 06:45:00 +0000

    implement secure_command() - secure operation execution

--- tee/Enclave/Enclave.cpp
+++ tee/Enclave/Enclave.cpp
@@ -10,6 +10,7 @@
 #include "errors.h"
 #include "state.h"
 #include "utils.h"
+#include "network.h"
 
 #define SGX_AESGCM_MAC_SIZE 16 // bytes
 #define SGX_AESGCM_IV_SIZE 12 // bytes
@@ -103,6 +104,48 @@ int ecall_create_channel(const char* tx_id, int tx_id_len, unsigned int tx_index
     return NO_ERROR;
 }
 
+// operation function for secure_command
+// ecall_create_channel() function should be removed later
+int secure_create_channel(const char* tx_id, int tx_id_len, unsigned int tx_index) {
+    
+    // 
+    // TODO: BITCOIN
+    // get this tx info (receiver) & compare the tx receiver vs rouTEE owner key
+    // 
+    // temp code
+    string receiver_addr = "";
+    if (receiver_addr != state.owner_address) {
+        return ERR_INVALID_RECEIVER;
+    }
+
+    // 
+    // TODO: BITCOIN
+    // get this tx info (ex. sender address & amount)
+    //
+    // temp code
+    string sender_addr = string(tx_id, tx_id_len) + "_" + long_long_to_string(tx_index);
+    unsigned long long amount = 100000000;
+
+    // check the user exists
+    map<string, Account*>::iterator iter = state.users.find(sender_addr);
+    if (iter == state.users.end()) {
+        // sender is not in the state, create new account
+        Account* acc = new Account;
+        acc->balance = 0;
+        acc->nonce = 0;
+        state.users[sender_addr] = acc;
+    }
+
+    // set user's balance
+    state.users[sender_addr]->balance += amount;
+
+    // increase state id
+    state.stateID++;
+    
+    printf("new channel created with rouTEE -> user: %s / balance: %llu\n", sender_addr.c_str(), amount);
+    return NO_ERROR;
+}
+
 void ecall_print_state() {
     // print all the state: all users' address and balance
     printf("    owner address: %s\n", state.owner_address.c_str());
@@ -125,6 +168,45 @@ void ecall_print_state() {
 }
 
 int ecall_settle_balance(const char* receiver_address, int receiver_addr_len) {
+    //
+    // TODO: BITCOIN
+    // check authority to get paid the balance (ex. receiver's signature with settlement params)
+    // if (no authority to get balance) {
+    //     return ERR_NO_AUTHORITY;
+    // }
+    //
+
+    // check the receiver has more than 0 balance
+    string receiver_addr = string(receiver_address, receiver_addr_len);
+    map<string, Account*>::iterator iter = state.users.find(receiver_addr);
+    if (iter == state.users.end() || iter->second->balance == 0) {
+        // receiver is not in the state || has no balance
+        return ERR_NOT_ENOUGH_BALANCE;
+    }
+
+    // push new settle request
+    state.settle_requests.push_back(SettleRequest());
+    state.settle_requests.back().address = receiver_addr;
+    state.settle_requests.back().balance = iter->second->balance;
+
+    // set user's account
+    printf("user %s requests settlement: %llu satoshi\n", receiver_addr.c_str(), iter->second->balance);
+    state.users[receiver_addr]->balance = 0;
+    state.users[receiver_addr]->nonce++; // prevent payment replay attack    
+
+    //
+    // TODO: commit pending fee to rouTEE operator (= fee address)
+    //
+
+    // increase state id
+    state.stateID++;
+
+    return NO_ERROR;
+}
+
+// operation function for secure_command
+// ecall_settle_balance() function should be removed later
+int secure_settle_balance(const char* receiver_address, int receiver_addr_len) {
     //
     // TODO: BITCOIN
     // check authority to get paid the balance (ex. receiver's signature with settlement params)
@@ -253,6 +335,59 @@ int ecall_do_multihop_payment(const char* sender_address, int sender_addr_len, c
     return NO_ERROR;
 }
 
+// operation function for secure_command
+// ecall_do_multihop_payment() function should be removed later
+int secure_do_multihop_payment(const char* sender_address, int sender_addr_len, const char* receiver_address, int receiver_addr_len, unsigned long long amount, unsigned long long fee) {
+    //
+    // TODO: BITCOIN
+    // check authority to send (ex. sender's signature with these params)
+    // if (no authority to send) {
+    //     return ERR_NO_AUTHORITY;
+    // }
+    //
+
+    // check the sender has more than amount + fee to send
+    string sender_addr = string(sender_address, sender_addr_len);
+    map<string, Account*>::iterator iter = state.users.find(sender_addr);
+    if (iter == state.users.end() || iter->second->balance < amount + fee) {
+        // sender is not in the state || has not enough balance
+        return ERR_NOT_ENOUGH_BALANCE;
+    }
+
+    // check routing fee
+    if (fee < state.routing_fee) {
+        return ERR_NOT_ENOUGH_FEE;
+    }
+
+    // check the receiver exists
+    string receiver_addr = string(receiver_address, receiver_addr_len);
+    iter = state.users.find(receiver_addr);
+    if (iter == state.users.end()) {
+        // receiver is not in the state, create new account
+        Account* acc = new Account;
+        acc->balance = 0;
+        acc->nonce = 0;
+        state.users[receiver_addr] = acc;
+    }
+
+    // move balance
+    state.users[sender_addr]->balance -= (amount + fee);
+    state.users[receiver_addr]->balance += amount;
+
+    // set pending fees
+    state.pending_fees[sender_addr] += fee/2;
+    state.pending_fees[receiver_addr] += fee - fee/2;
+
+    // increase sender's nonce
+    state.users[sender_addr]->nonce++;
+
+    // increase state id
+    state.stateID++;
+
+    printf("send %llu from %s to %s / fee %llu to %s\n", amount, sender_addr.c_str(), receiver_addr.c_str(), fee, state.fee_address.c_str());
+    return NO_ERROR;
+}
+
 int ecall_insert_block(const char* block, int block_len) {
     // 
     // TODO: BITCOIN
@@ -260,10 +395,50 @@ int ecall_insert_block(const char* block, int block_len) {
     // 
 }
 
+int make_encrypted_response(int result_error_index, sgx_aes_gcm_128bit_key_t *session_key, char* encrypted_response, int* encrypted_response_len) {
+    // return encrypted response to client
+    const char* response_msg = error_to_msg(result_error_index);
+    printf("response_msg: %s\n", response_msg);
+    uint8_t *response = (uint8_t *) response_msg;
+    size_t len = strlen(response_msg);
+	uint8_t p_dst[BUFLEN] = {0};
+
+	// Generate the IV (nonce)
+	sgx_read_rand(p_dst + SGX_AESGCM_MAC_SIZE, SGX_AESGCM_IV_SIZE);
+
+    // encrypt the response
+	sgx_status_t status = sgx_rijndael128GCM_encrypt(
+		session_key,
+		response, len, 
+		p_dst + SGX_AESGCM_MAC_SIZE + SGX_AESGCM_IV_SIZE,
+		p_dst + SGX_AESGCM_MAC_SIZE,
+        SGX_AESGCM_IV_SIZE,
+		NULL, 0,
+		(sgx_aes_gcm_128bit_tag_t *) (p_dst)
+    );
+
+    // check encryption result
+    if (status != SGX_SUCCESS) {
+        // encryption failed: abnormal case
+        // this cannot be happened in ordinary situations
+        // SGX_ERROR_INVALID_PARAMETER || SGX_ERROR_OUT_OF_MEMORY || SGX_ERROR_UNEXPECTED
+        return ERR_ENCRYPT_FAILED;
+    }
+
+    // copy encrypted response to outside buffer
+    *encrypted_response_len = SGX_AESGCM_MAC_SIZE+SGX_AESGCM_IV_SIZE+len;
+	memcpy(encrypted_response, p_dst, *encrypted_response_len);
+
+    return NO_ERROR;
+}
+
 int ecall_secure_command(const char* sessionID, int sessionID_len, const char* encrypted_cmd, int encrypted_cmd_len, char* encrypted_response, int* encrypted_response_len) {
 
     // error index of this ecall function
     int result_error_index;
+
+    // error index of encryption result
+    int encryption_result;
 
     // decrypt cmd
     uint8_t *encMessage = (uint8_t *) encrypted_cmd;
@@ -288,7 +463,14 @@ int ecall_secure_command(const char* sessionID, int sessionID_len, const char* e
     );
 
     if (status != SGX_SUCCESS) {
-        result_error_index = ERR_SGX_ERROR_DECRYPT_FAILED;
+        // return encrypted response to client
+        // make encrypted response 
+        // and return NO_ERROR to hide the ecall result from rouTEE host
+        encryption_result = make_encrypted_response(ERR_DECRYPT_FAILED, session_key, encrypted_response, encrypted_response_len);
+        if (encryption_result != NO_ERROR) {
+            return ERR_ENCRYPT_FAILED;
+        }
+        return NO_ERROR;
     }
     
     char *decMessage = (char *) malloc((decMessageLen+1)*sizeof(char));
@@ -296,50 +478,72 @@ int ecall_secure_command(const char* sessionID, int sessionID_len, const char* e
     decMessage[decMessageLen] = '\0';
     printf("decrypted cmd: %s\n", decMessage);
 
-    //
-    // TODO: execute decrypted cmd
-    //
+    // execute decrypted cmd
 
-    // test code
-    // for (long long int i=0;i<10000000000;i++){
-    //     // just time consuming work
-    //     // almost same as --> sleep(10 sec);
-    //     ;
-    // }
+    // parse the command to get parameters
+    vector<string> params;
+    string cmd = string(decMessage, decMessageLen);
+    split(cmd, params, ' ');
 
+    // find appropriate operation
+    char operation = params[0][0];
+    int operation_result;
+    if (operation == OP_CREATE_CHANNEL) {
+        // add deposit request
+        if (params.size() != 3) {
+            // invalid parameter count
+            operation_result = ERR_INVALID_PARAMS;
+        }
+        else {
+            // get parameters
+            string tx_id = params[1];
+            unsigned int tx_index = strtoul(params[2].c_str(), NULL, 10);
 
+            // execute operation
+            operation_result = secure_create_channel(tx_id.c_str(), tx_id.length(), tx_index);
+        }
+    }
+    else if (operation == OP_SETTLE_BALANCE) {
+        // settle balance request
+        if (params.size() != 2) {
+            // invalid parameter count
+            operation_result = ERR_INVALID_PARAMS;
+        }
+        else {
+            // get parameters
+            string receiver_address = params[1];
 
+            // execute operation
+            operation_result = secure_settle_balance(receiver_address.c_str(), receiver_address.length());
+        }
+    }
+    else if (operation == OP_DO_MULTIHOP_PAYMENT) {
+        // multi-hop payment request
+        if (params.size() != 5) {
+            // invalid parameter count
+            operation_result = ERR_INVALID_PARAMS;
+        }
+        else {
+            // get parameters
+            string sender_address = params[1];
+            string receiver_address = params[2];
+            unsigned long long amount = strtoul(params[3].c_str(), NULL, 10);
+            unsigned long long fee = strtoul(params[4].c_str(), NULL, 10);
 
-
-    // return encrypted response to client
-    const char* response_msg = error_to_msg(result_error_index);
-    printf("response_msg: %s\n", response_msg);
-    uint8_t *response = (uint8_t *) response_msg;
-    size_t len = strlen(response_msg);
-	p_dst[BUFLEN] = {0};
-
-	// Generate the IV (nonce)
-	sgx_read_rand(p_dst + SGX_AESGCM_MAC_SIZE, SGX_AESGCM_IV_SIZE);
-
-    // encrypt
-	status = sgx_rijndael128GCM_encrypt(
-		session_key,
-		response, len, 
-		p_dst + SGX_AESGCM_MAC_SIZE + SGX_AESGCM_IV_SIZE,
-		p_dst + SGX_AESGCM_MAC_SIZE,
-        SGX_AESGCM_IV_SIZE,
-		NULL, 0,
-		(sgx_aes_gcm_128bit_tag_t *) (p_dst)
-    );
-
-    if (status != SGX_SUCCESS) {
-        return ERR_SGX_ERROR_ENCRYPT_FAILED;
+            // execute operation
+            operation_result = secure_do_multihop_payment(sender_address.c_str(), sender_address.length(), receiver_address.c_str(), receiver_address.length(), amount, fee);
+        }
+    }
+    else {
+        // invalid opcode
+        operation_result = ERR_INVALID_OP_CODE;
     }
 
-    // copy encrypted response to outside buffer
-    *encrypted_response_len = SGX_AESGCM_MAC_SIZE+SGX_AESGCM_IV_SIZE+len;
-	memcpy(encrypted_response, p_dst, *encrypted_response_len);
-
+    // encrypt the response for client & return NO_ERROR to hide the ecall result from rouTEE host
+    encryption_result = make_encrypted_response(operation_result, session_key, encrypted_response, encrypted_response_len);
+    if (encryption_result != NO_ERROR) {
+        return ERR_ENCRYPT_FAILED;
+    }
     return NO_ERROR;
 }
 
@@ -360,7 +564,7 @@ int ecall_make_owner_key(char* sealed_owner_private_key, int* sealed_key_len) {
     sgx_sealed_data_t *sealed_key_buffer = (sgx_sealed_data_t *) malloc(sealed_data_size);
     sgx_status_t status = sgx_seal_data(0, NULL, (uint32_t)strlen(random_private_key), (uint8_t *) random_private_key, sealed_data_size, sealed_key_buffer);
     if (status != SGX_SUCCESS) {
-        return ERR_SGX_ERROR_SEAL_FAILED;
+        return ERR_SEAL_FAILED;
     }
 
     // copy sealed key to the app buffer
@@ -378,7 +582,7 @@ int ecall_load_owner_key(const char* sealed_owner_private_key, int sealed_key_le
     uint8_t unsealed_private_key[unsealed_key_length];
     sgx_status_t status = sgx_unseal_data((const sgx_sealed_data_t *) sealed_owner_private_key, NULL, 0, unsealed_private_key, &unsealed_key_length);
     if (status != SGX_SUCCESS) {
-        return ERR_SGX_ERROR_UNSEAL_FAILED;
+        return ERR_UNSEAL_FAILED;
     }
 
     // set owner_private_key
@@ -407,7 +611,7 @@ int ecall_seal_state(char* sealed_state, int* sealed_state_len) {
     sgx_sealed_data_t *sealed_state_buffer = (sgx_sealed_data_t *) malloc(sealed_data_size);
     sgx_status_t status = sgx_seal_data(0, NULL, (uint32_t)state_str.length(), (uint8_t *) state_str.c_str(), sealed_data_size, sealed_state_buffer);
     if (status != SGX_SUCCESS) {
-        return ERR_SGX_ERROR_SEAL_FAILED;
+        return ERR_SEAL_FAILED;
     }
 
     // copy sealed state to the app buffer
@@ -425,7 +629,7 @@ int ecall_load_state(const char* sealed_state, int sealed_state_len) {
     uint8_t unsealed_state[unsealed_state_length];
     sgx_status_t status = sgx_unseal_data((const sgx_sealed_data_t *) sealed_state, NULL, 0, unsealed_state, &unsealed_state_length);
     if (status != SGX_SUCCESS) {
-        return ERR_SGX_ERROR_UNSEAL_FAILED;
+        return ERR_UNSEAL_FAILED;
     }
 
     // load global state
