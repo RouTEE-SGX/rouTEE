f6fd5e3b122c58b60eef0ab2cb2b3ae228659bf8
jmlee <junmo.lee@snu.ac.kr>
2021-12-05 07:07:07 +0000

    minor fix

--- tee/Enclave/network.h
+++ tee/Enclave/network.h
@@ -6,28 +6,28 @@
 //
 
 // opcodes for test
-#define OP_PUSH_A   97 // a (cmd: a) (for ping test, round trip time test)
+#define OP_PUSH_A   97                  // a (cmd: a) (for ping test, round trip time test)
 
 // opcodes for rouTEE host
-#define OP_SET_ROUTING_FEE  104 // h (cmd: h fee)
+#define OP_SET_ROUTING_FEE  104         // h (cmd: h fee)
 #define OP_SET_ROUTING_FEE_ADDRESS  105 // i (cmd: i fee_addr)
 #define OP_MAKE_SETTLE_TRANSACTION  110 // n (cmd: n)
-#define OP_INSERT_BLOCK 111 // o (cmd: o block_number)
-#define OP_INSERT_BLOCK_HEADER 100 // d (cmd: d block_number)
-#define OP_SETTLE_ROUTING_FEE 118 // v (cmd: v amount)
-#define OP_SYNC_WITH_BLOCKCHAIN 98 // b (cmd: b)
+#define OP_INSERT_BLOCK 111             // o (cmd: o block_number)
+#define OP_INSERT_BLOCK_HEADER 100      // d (cmd: d block_number)
+#define OP_SETTLE_ROUTING_FEE 118       // v (cmd: v amount)
+#define OP_SYNC_WITH_BLOCKCHAIN 98      // b (cmd: b)
 
 // opcodes for rouTEE users
-#define OP_SECURE_COMMAND 112 // p (cmd: p sessionID encrypted_command_data)
-#define OP_ADD_USER 118 // v (cmd: v settle_addr public_key)
-#define OP_GET_READY_FOR_DEPOSIT  106 // j (cmd: j sender_address settle_address)
-#define OP_SETTLE_BALANCE   108 // l (cmd: l receiver_addr amount)
-#define OP_DO_MULTIHOP_PAYMENT  109 // m (cmd: m sender_addr receiver_addr amount fee)
-#define OP_UPDATE_LATEST_SPV_BLOCK 113 // q (cmd: q user_address blocknumber)
+#define OP_SECURE_COMMAND 112           // p (cmd: p sessionID encrypted_command_data)
+#define OP_ADD_USER 118                 // v (cmd: v settle_addr public_key)
+#define OP_GET_READY_FOR_DEPOSIT  106   // j (cmd: j sender_address settle_address)
+#define OP_SETTLE_BALANCE   108         // l (cmd: l receiver_addr amount)
+#define OP_DO_MULTIHOP_PAYMENT  109     // m (cmd: m sender_addr receiver_addr amount fee)
+#define OP_UPDATE_LATEST_SPV_BLOCK 113  // q (cmd: q user_address blocknumber)
 
 // opcodes for rouTEE debugging
-#define OP_PRINT_STATE  107 // k (cmd: k)
-#define OP_INSERT_DEPOSIT_TX 114 // r (cmd: r sender_address amount block_number)
-#define OP_INSERT_SETTLE_TX 117 // u (cmd: u)
+#define OP_PRINT_STATE  107             // k (cmd: k)
+#define OP_INSERT_DEPOSIT_TX 114        // r (cmd: r sender_address amount block_number)
+#define OP_INSERT_SETTLE_TX 117         // u (cmd: u)
 
 #endif  // _NETWORK_H_
